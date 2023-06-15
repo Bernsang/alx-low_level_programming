1a4ccbe25feebe83fead5f3eac73964235783627
Bernadette <bernadettesang@gmail.com>
2023-06-15 15:27:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main -puts function is used in this program
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
