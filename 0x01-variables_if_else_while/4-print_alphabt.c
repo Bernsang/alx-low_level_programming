2dd7ad704c60d0de86351ad16ff9ed2fa393bcc6
Bernadette <bernadettesang@gmail.com>
2023-06-17 06:39:04 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - prints all the letters except q and e
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z' low++ | grep - v[qe])
+	{
+		putchar (low);
+	}
+	return (0);
+}
