13562088af192899dc565c460019397335115cbc
Bernadette Sang <bernadettesang@gmail.com>
2023-06-24 01:31:24 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,9 +8,11 @@
   */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	char c;
+	c = 'A';
+	printf("c: %d\n", c, _isupper(c));
 		return (1);
-	else
-		
-		return (0);
+	c = 'a';
+	printf("c: %d\n", c, _isupper(c));
+	    return (0);
 }
